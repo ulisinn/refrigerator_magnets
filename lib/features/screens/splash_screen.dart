ddb3c669f27d41d02618e7a70c9050477218c085
ulrichsi-hrbl <ulrichsi@herbalife.com>
2023-10-17 11:44:25 -0700

    layout fixed

--- lib/features/screens/splash_screen.dart
+++ lib/features/screens/splash_screen.dart
@@ -8,50 +8,54 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Splash/Home Screen',
-            style: TextStyle(fontSize: 24),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go(AppRoute.login.name);
-            },
-            child: const Text('Go to Login'),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go(AppRoute.createAccount.name);
-            },
-            child: const Text('Go to Create Account'),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go(AppRoute.logout.name);
-            },
-            child: const Text('Go to Logout'),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go(AppRoute.landing.name);
-            },
-            child: const Text('Go to Landing Screen'),
-          ),
-        ],
+      body: SizedBox(
+        // set the height property to take the screen width
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Splash/Home Screen',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.go(AppRoute.login.name);
+              },
+              child: const Text('Go to Login'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.go(AppRoute.createAccount.name);
+              },
+              child: const Text('Go to Create Account'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.go(AppRoute.logout.name);
+              },
+              child: const Text('Go to Logout'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.go(AppRoute.landing.name);
+              },
+              child: const Text('Go to Landing Screen'),
+            ),
+          ],
+        ),
       ),
     );
   }
